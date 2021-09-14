abe83bff872dc7d3f38b582142733320dbef2a46
kangbo <841331654@qq.com>
2021-09-14 09:48:17 +0800

    add

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import {render} from "@testing-library/react"
 import Button  from './button';
 
-test("our first react test case",()=>{
-  const wrapper = render(<Button>nice</Button>)
-  const element = wrapper.queryAllByText('nice')
-  expect(element).toBeTruthy()
+
+
+describe("test button component",()=>{
+  it('should render the correct default button',()=>{
+    const wrapper = render(<Button>nice</Button>)
+    const element = wrapper.getByText('nice')
+    expect(element).toBeTruthy()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+
+  })
+
+  it("should render the correct component based on different props",()=>{
+
+  })
+
+  it("should render a link when btnType equals link and href is provided",()=>{
+    
+  })
+
+  it("should render disabled button when disabled set to true",()=>{
+    
+  })
 })
